8217df6750e6b914effb4983b47a2eec5be7c431
peterrr1 <87699844+peterrr1@users.noreply.github.com>
2022-05-11 21:34:34 +0200

    Add files via upload

--- filekezeles.cpp
+++ filekezeles.cpp
@@ -1,49 +1,65 @@
-#include "filekezeles.h"
+#include "filekezeles.hpp"
 #include <sstream>
 
+
+
 void File::fileBeolvas(Lista& lista) {
-	std::fstream file;
 
-	file.open("telefonkonyv.txt", std::ios::in);
-	if (!file) {
-		throw std::runtime_error("\nHiba a fajl megnyitasa kozben!\n");
-	}
-	else {
-		std::string sor;
-		while (std::getline(file, sor)) {
-
-			std::istringstream ss(sor);
-
-			std::string tmpNev, tmpBecNev, tmpCim, tmpEmail, tmpMSzam, tmpPSzam;
-
-			std::getline(ss,tmpNev, ':');
-			std::getline(ss, tmpBecNev, ':');
-			std::getline(ss, tmpCim, ':');
-			std::getline(ss, tmpEmail, ':');
-			std::getline(ss, tmpMSzam, ':');
-			std::getline(ss, tmpPSzam, ':');
-
-			Adatok ujAdat(tmpNev, tmpBecNev, tmpCim, tmpEmail, tmpMSzam, tmpPSzam);
-
-			lista.hozzaad(ujAdat);
-		}
-	}
-	file.close();
+    std::fstream file;
+    
+    file.open("telefonkonyv.txt", std::ios::in | std::ios::app);
+    try {
+        if (!file)
+            throw std::runtime_error("\nHiba a fajl megnyitasa kozben.\n");
+        else {
+            std::string sor;
+            
+            while (std::getline(file, sor)) {
+                
+                std::stringstream ss(sor);
+                
+                std::string tmpNev, tmpBecenev, tmpLakcim, tmpEmail, tmpMunkSzam, tmpPrivSzam;
+                
+                std::getline(ss, tmpNev, ':');
+                std::getline(ss, tmpBecenev, ':');
+                std::getline(ss, tmpLakcim, ':');
+                std::getline(ss, tmpEmail, ':');
+                std::getline(ss, tmpMunkSzam, ':');
+                std::getline(ss, tmpPrivSzam, ':');
+                
+                Adatok ujAdat(tmpNev, tmpBecenev, tmpLakcim, tmpEmail, tmpMunkSzam, tmpPrivSzam);
+                
+                lista.hozzafuz(ujAdat);
+                
+            }
+        }
+    }
+    catch (std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+    }
+    file.close();
 }
 
-void File::fileKiir(Lista& lista) {
-	std::fstream file;
 
-	file.open("telefonkonyv.txt", std::ios::out);
-	if (!file){
-		throw std::runtime_error("\nHiba a fajl megnyitasa kozben!\n");
-	}
-	else {
-		for (Lista::Elem* tmp = lista.elso; tmp != NULL; tmp = tmp->kov){
-			file << tmp->adat.getNev() << ':' << tmp->adat.getBecenev() << ':' <<
-			tmp->adat.getLakcim() << ':' << tmp->adat.getEmail() << ':' <<
-			tmp->adat.getMunkahelyi() << ':' << tmp->adat.getPrivat() << '\n';
-		}
-		file.close();
-	}
-}
+
+void File::fileKiir(Lista& lista) const {
+    
+    std::fstream file;
+    
+    file.open("telefonkonyv.txt", std::ios::out);
+    try {
+        if (!file)
+            throw std::runtime_error("\nHiba a fajl megnyitasa kozben.\n");
+        else {
+            for (Lista::Elem* tmp = lista.elso; tmp != NULL; tmp = tmp->kov){
+                file << tmp->adat.getNev() << ':' << tmp->adat.getBecenev() << ':' <<
+                tmp->adat.getLakcim() << ':' << tmp->adat.getEmail() << ':' <<
+                tmp->adat.getMunkSzam() << ':' << tmp->adat.getPrivSzam() << '\n';
+            }
+        }
+    }
+    catch (std::runtime_error& err) {
+        std::cerr << err.what() << std::endl;
+    }
+    file.close();
+};
